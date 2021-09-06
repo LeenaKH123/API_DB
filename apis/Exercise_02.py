6ae71f7350a7827c16c1b6841a56a9fe8080a6bb
Leena <engleena@yahoo.com>
2021-09-06 19:53:33 +0300

    Initial Commit

--- apis/Exercise_02.py
+++ apis/Exercise_02.py
@@ -1,5 +1,18 @@
-'''
+"""
 Building on the previous example, create a list of all of the emails of the users and print
 the list to the console.
 
-'''
+"""
+import requests
+import re
+
+regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users")
+print(response.status_code)
+print(response.encoding)
+# print(response.content)
+# print(response.json())
+users = []
+for email in response.content:
+    if re.search(regex, email):
+        print(email)
