25b218fe75379b8b724d4764c0722321d87083c0
root <root@DESKTOP-G07NF43.localdomain>
2021-09-08 14:28:42 +0300

    post request

--- apis/Exercise_03.py
+++ apis/Exercise_03.py
@@ -8,3 +8,13 @@ and create a user with your information.
 Make a GET request to confirm that your user information has been saved.
 
 '''
+import requests
+import json
+NewUser = {'id': 100, 'first_name': 'Leena', 'last_name': 'K', 'email': 'engleena@outlook.com'}
+url = 'http://demo.codingnomads.co:8080/tasks_api/users'
+x = requests.post(url, json= NewUser)
+print(x.status_code, x.reason)
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users")
+output = (response.json())
+output1 = output["data"]
+print(output1)
