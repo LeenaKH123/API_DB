866d9215a9516bc6c0752d0833c62cfb473b254d
root <root@DESKTOP-G07NF43.localdomain>
2021-09-08 14:41:31 +0300

    delete request worked

--- apis/Exercise_04.py
+++ apis/Exercise_04.py
@@ -4,3 +4,13 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+import requests
+import json
+NewUser = {'first_name': 'Leena2', 'last_name': 'K2', 'email': 'engleena2@outlook.com'}
+url = 'http://demo.codingnomads.co:8080/tasks_api/users'
+x = requests.put(url, json=NewUser)
+print(x.status_code, x.reason)
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users")
+output = (response.json())
+output1 = output["data"]
+print(output1)
--- apis/Exercise_05.py
+++ apis/Exercise_05.py
@@ -4,3 +4,8 @@ Write a program that makes a DELETE request to remove the user your create in a 
 Again, make a GET request to confirm that information has been deleted.
 
 '''
+import requests
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+response = requests.delete(base_url + "/419")
+print(response.status_code)
