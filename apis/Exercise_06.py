75d40e13ddc084a70eaf7bcb04effc59ebc91c62
Leena <engleena@yahoo.com>
2021-09-15 21:06:34 +0300

    API's in progress

--- apis/Exercise_06.py
+++ apis/Exercise_06.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Create an application that interfaces with the user via the CLI - prompt the user with a menu such as:
 
@@ -12,44 +12,40 @@ Please select from the following options (enter the number of the action you'd l
 7) Delete a task (DELETE)
 
 It is your responsibility to build out the application to handle all menu options above.
-
-
-'''
+"""
 import requests
 import json
+
 url = "http://demo.codingnomads.co:8080/tasks_api/users"
-selection = input("Please select from the following options (enter the number of the action you would like to take) \n 1) create a new account \n 2) View all your tasks \n 3) View your completed tasks \n 4) View only your incomplete tasks \n 5) Create a new task \n 6) Update an existing task \n 7) Delete a task \n")
-if selection == '1':
+selection = input(
+    "Please select from the following options (enter the number of the action you would like to take) \n 1) create a new account \n 2) View all your tasks \n 3) View your completed tasks \n 4) View only your incomplete tasks \n 5) Create a new task \n 6) Update an existing task \n 7) Delete a task \n"
+)
+if selection == "1":
     print("We will create a new account for you ")
-    firstName = input ("Type your First Name ")
-    lastName = input ("Type your Last Name ")
-    email = input ("Type in your email ")
-    NewUser = {'first_name': firstName, 'last_name': lastName, 'email': email}
-    x = requests.post(url, json= NewUser)
+    firstName = input("Type your First Name ")
+    lastName = input("Type your Last Name ")
+    email = input("Type in your email ")
+    NewUser = {"first_name": firstName, "last_name": lastName, "email": email}
+    x = requests.post(url, json=NewUser)
 
-elif selection == '2':
-    email = input ("Please type your email to show all your tasks ") 
-    response = requests.get(url)
-    output = (response.json())
+elif selection == "2":
+    userid = input("Please type your userid to show all your tasks ")
+    response = requests.get(url + "/" + userid + "/" + "tasks?complete=-1")
+    output = response.json()
     outputData = output["data"]
-   
+
     for item in outputData:
-        if item['email'] == email:
+        print(item)
+        if item["userId"] == userid:
             print(item)
-        else:
-            print("Your email doesn't exist")
+        # else:
+        #     print("Your ID doesn't exist")
 
-elif selection == '3':
+elif selection == "3":
     print("I dont know what to do here")
 
-elif selection == '4':
+elif selection == "4":
     print("I dont know what to do here")
-
-
-
-
-
-    
 
 
 # elif selection == '3':
--- databases/Exercise_01.py
+++ databases/Exercise_01.py
@@ -1,17 +1,17 @@
-'''
+"""
 
 All of the following exercises should be done using sqlalchemy.
 
 Using the provided database schema, write the necessary code to print information about the film and category table.
 
-'''
+"""
 import sqlalchemy
 
 
-engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/sakila')
+engine = sqlalchemy.create_engine("mysql+pymysql://root:MyNewPass@localhost/sakila")
 connection = engine.connect()
 metadata = sqlalchemy.MetaData()
-actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+actor = sqlalchemy.Table("actor", metadata, autoload=True, autoload_with=engine)
 
 print(actor.columns.keys())
-print(repr(metadata.tables['actor']))
+print(repr(metadata.tables["actor"]))
