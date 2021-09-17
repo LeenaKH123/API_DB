ed42616a818809422e1d37a9fd557599befdce1f
Leena <engleena@yahoo.com>
2021-09-17 16:12:28 +0300

    dbs in progress

--- databases/Exercise_01.py
+++ databases/Exercise_01.py
@@ -7,10 +7,11 @@ Using the provided database schema, write the necessary code to print informatio
 """
 import sqlalchemy
 
-engine = sqlalchemy.create_engine("mysql+pymysql://leena:leena@localhost/sakila")
+engine = sqlalchemy.create_engine("mysql+pymysql://root:MyNewPass@localhost/sakila")
 connection = engine.connect()
 metadata = sqlalchemy.MetaData()
-actor = sqlalchemy.Table("actor", metadata, autoload=True, autoload_with=engine)
+film = sqlalchemy.Table("film", metadata, autoload=True, autoload_with=engine)
+category = sqlalchemy.Table("category", metadata, autoload=True, autoload_with=engine)
 
-print(actor.columns.keys())
-print(repr(metadata.tables["actor"]))
+print(repr(metadata.tables["film"]))
+print(repr(metadata.tables["category"]))
